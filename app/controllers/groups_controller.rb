0cb12fb678ab1d4b2af38cec6475ca5a2c3397ce
andy <s13119229@stu.edu>
2015-04-15 15:52:08 +0800

    3.0 done.

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,7 +5,8 @@ class GroupsController < ApplicationController
 	end
 
 	def show
-		
+		@group = Group.find(params[:id])
+		@posts = @group.posts
 	end
 
 	def new
@@ -13,7 +14,7 @@ class GroupsController < ApplicationController
 	end
 
 	def edit
-		
+		@group = Group.find(params[:id])
 	end
 
 	def create
@@ -27,11 +28,19 @@ class GroupsController < ApplicationController
 	end
 
 	def update
-		
+		@group = Group.find(params[:id])
+
+        if @group.update(group_params)
+          redirect_to groups_path, notice: "修改討論版成功"
+        else
+          render :edit
+        end
 	end
 
 	def destroy
-		
+		@group = Group.find(params[:id])
+        @group.destroy
+         redirect_to groups_path, alert: "討論版已刪除"
 	end
 
 	private
